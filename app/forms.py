ced652d04d133bb39780c087f1fed74276450c48
user <lthrncube@gmail.com>
2020-06-04 23:12:58 +0200

    updated formsx

--- app/forms.py
+++ app/forms.py
@@ -5,11 +5,10 @@ from wtforms.validators import DataRequired, Email
 class RegForm(FlaskForm):
 	name = StringField('Name', validators=[DataRequired()])
 	surname = StringField('Surname', validators=[DataRequired()])
-	sect = StringField('Section', validators=[DataRequired()])
-	email = StringField('Email', validators=[DataRequired(), Email()])
+	email = StringField('Email', validators=[DataRequired()])
 	password_hash = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Sign up')
 
 class ReqLeave(FlaskForm):
-	d = IntegerField('Enter num of days', validators=[DataRequired])
+	d = IntegerField('Enter num of days', validators=[DataRequired()])
 	submit = SubmitField('Ok')
