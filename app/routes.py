d179c9a1c6223d6f2c00fee4e9087f7b1e3ea5f4
luther <lthrncube@gmail.com>
2020-09-22 16:13:39 +0200

    Added api

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, request, redirect, jsonify
+from flask import render_template, url_for, request, redirect, jsonify, send_file
 from app import app, db
 from app.models import User
 from app.forms import RegForm, ReqLeave
+
 
 @app.route('/')
 @app.route('/index')
@@ -18,7 +19,17 @@ def register():
 @app.route('/api', methods=['POST'])
 def create():
 	cont = request.json
-	usr = User(name=cont['name'], surname=cont['surname'])
+	usr = User(name=cont['pack'], title=cont['title'], text=cont['text'])
 	db.session.add(usr)
 	db.session.commit()
 	return jsonify(cont)
+
+@app.route('/d/<name>', methods=['GET'])
+def dd(name):
+	upload = 'static/' + name
+	return send_file(upload, as_attachment=True)
+
+@app.route('/view')
+def v():
+	view = User.query.all()
+	return render_template('view.html', view=view)
